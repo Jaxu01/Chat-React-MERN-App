12be15520bad67199882cec4548b3aeca853abd3
JakubP <jakubpiwtorak@gmail.com>
2022-08-16 16:02:30 +0200

    Chat React MERN App

--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require('path');
 require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const port = process.env.PORT || 5000;
 // get driver connection
 const dbo = require("./db/conn");
  
